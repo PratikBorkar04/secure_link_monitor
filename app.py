d684218caeec23bf5e676b0967f7955400293345
Pratik Borkar <pratikab01@gmail.com>
2024-01-25 15:57:41 +0530

    Project structure Created'

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import pandas as pd
+from src.exception import securelinkException
+import sys
 df = pd.DataFrame([1,2,3,4])
-print(df)
+a = 10
+try:
+    a/0
+except Exception as ex:
+    raise securelinkException(ex,sys) from ex
